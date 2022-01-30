0df5ba1be7f5845fb6c976d6e8132fee56c20126
Eduard Loktev <lwqtbdwrd@gmail.com>
2022-01-30 11:37:21 +0200

    change css , add history cleaner

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
-
-
-const numsBtn = document.querySelectorAll('.btn');
+const allBtn = document.querySelectorAll('.btn');
+const numsBtn = document.querySelectorAll('.btn_type_num');
 const calcInputs = document.querySelector('.calc__inputs');
 const cleanScreenBtn = document.querySelector('.calc__delete');
 const resultImg = document.querySelector('.giphy-embed')
 const sideInputScreen = document.querySelector('.calc__screen-for-allinputs');
 const equalBtn = document.querySelector('.calc__equal');
-const operators = document.querySelectorAll('.operator')
+const operators = document.querySelectorAll('.btn_type_operator')
 const templateElement = document.querySelector('.history-element')
-const historyContainer = document.querySelector('.history');
-const zeroBtn = document.querySelector('.calc__zero')
+const historyContainer = document.querySelector('.history__solutions');
+const zeroBtn = document.querySelector('.calc__zero');
+const deleteHsrIcon = document.querySelector('.history__trash-icon');
 
 
-let operator = '';
+let button = '';
 let counter = 0
 let secondInput = 0;
 let firstInput = 0;
 let solution = 0;
 
-// function handleZerobtn() {
-//     if (calcInputs.textContent === '') {
-//         zeroBtn.classList.add('operator_is-disabled');
-//     }
-//     else {
-//         zeroBtn.classList.remove('operator_is-disabled');
-//     }
-// }
-// handleZerobtn();
+deleteHsrIcon.addEventListener('click', () => {
+    historyContainer.innerHTML = ''
+})
 
-function handleStartOperation() {
+function handleDisableBtn(btn) {
 
-    operators.forEach(operator => {
-        if (firstInput === 0 && secondInput === 0) {
-            operator.disabled = true;
-            equalBtn.disabled = true;
-            operator.classList.add('operator_is-disabled');
-        }
-        else {
-            operator.disabled = false;
-            equalBtn.disabled = false;
-            operator.classList.remove('operator_is-disabled');
+    btn.disabled = true;
+    btn.classList.add('button_is-disabled');
 
-        }
-    })
-
+}
+function handleEnableBtn(btn) {
+    btn.disabled = false;
+    btn.classList.remove('button_is-disabled');
 }
 
 
-handleStartOperation();
 
+function handleDisableBtns(butsArray) {
+    butsArray.forEach((btn) => {
+        btn.disabled = true;
+        btn.classList.add('button_is-disabled');
+    })
+}
 
+function handleEnableBtns(butsArray) {
+    butsArray.forEach((btn) => {
+        btn.disabled = false;
+        btn.classList.remove('button_is-disabled');
+    })
+}
+handleDisableBtns(operators);
+handleDisableBtn(zeroBtn);
+handleDisableBtn(equalBtn);
 
 cleanScreenBtn.addEventListener('click', resetTheCalculator);
 
@@ -58,16 +59,20 @@ function cleanScreen() {
     calcInputs.textContent = '';
 
 }
+
 function resetTheCalculator() {
     sideInputScreen.textContent = ''
     cleanScreen();
-
-    operator = '';
+    button = '';
     counter = 0
     secondInput = 0;
     firstInput = 0;
     solution = 0;
-    handleStartOperation();
+    handleDisableBtns(operators)
+    handleEnableBtns(numsBtn)
+    handleDisableBtn(zeroBtn)
+    handleDisableBtn(equalBtn)
+
 }
 
 operators.forEach((operator) => {
@@ -82,7 +87,7 @@ operators.forEach((operator) => {
 })
 ////
 
-numsBtn.forEach((numBtn) => {
+allBtn.forEach((numBtn) => {
 
     numBtn.addEventListener('click', () => {
         console.log(numBtn.textContent)
@@ -97,6 +102,8 @@ numsBtn.forEach((numBtn) => {
                 calcInputs.textContent += parseInt(input);
                 secondInput = parseInt(calcInputs.textContent)
                 console.log(secondInput, 'second')
+                handleEnableBtns(operators)
+                handleEnableBtn(equalBtn);
 
 
 
@@ -105,19 +112,21 @@ numsBtn.forEach((numBtn) => {
 
                 calcInputs.textContent += parseInt(input);
                 firstInput = parseInt(calcInputs.textContent)
-                // handleDivideOperation();
-                handleStartOperation();
+                handleEnableBtns(operators)
+                handleEnableBtn(zeroBtn);
                 console.log(firstInput, 'first')
             }
 
         }
 
         else {
-            operator = numBtn.textContent;
+            button = numBtn.textContent;
             cleanScreen()
-            console.log(operator)
+            console.log(button)
             counter++
             console.log(counter)
+            handleDisableBtns(operators)
+            handleEnableBtns(numsBtn)
 
 
         }
@@ -129,23 +138,23 @@ numsBtn.forEach((numBtn) => {
 
 
 function handleDivideOperation() {
-    if (operator === '+') {
+    if (button === '+') {
 
         solution = firstInput + secondInput;
         console.log(solution)
         calcInputs.textContent = solution
     }
-    if (operator === 'x') {
+    if (button === 'x') {
         solution = firstInput * secondInput;
         console.log(solution)
         calcInputs.textContent = solution
     }
-    if (operator === '÷') {
+    if (button === '÷') {
         solution = firstInput / secondInput;
         console.log(solution)
         calcInputs.textContent = solution
     }
-    if (operator === '-') {
+    if (button === '-') {
         solution = firstInput - secondInput;
         console.log(solution)
         calcInputs.textContent = solution
@@ -153,17 +162,18 @@ function handleDivideOperation() {
     firstInput = solution;
     secondInput = 0;
     counter = 0;
+    handleDisableBtn(equalBtn);
 
 }
 equalBtn.addEventListener('click', () => {
     handleDivideOperation()
     sideInputScreen.textContent = sideInputScreen.textContent + equalBtn.textContent + solution;
-    // sideInputScreen.textContent = solution;
     const historyElement = templateElement.content.querySelector('.history__solution').cloneNode(true);
     console.log(historyElement)
     historyElement.textContent = sideInputScreen.textContent;
     historyContainer.appendChild(historyElement)
     sideInputScreen.textContent = solution
+    handleDisableBtns(numsBtn)
 
 
 })
@@ -171,10 +181,9 @@ equalBtn.addEventListener('click', () => {
 
 // todo list
 
-// handle zero when the screen is empty
-// handle operators when we click operator // or disable the operators or change the variabel
-// handle the solution // the solution cant be modified after we pres equal
-// handle solution so it will be appear when we click second operation for exapmle 1+1 and if we click to some operator again the user will see the solution in the screen 
+// handle buttons when we click button // or disable the buttons or change the variabel
+
+// handle solution so it will be appear when we click second operation for exapmle 1+1 and if we click to some button again the user will see the solution in the screen 
 
 
 
